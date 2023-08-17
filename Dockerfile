b6b196bc9a5ec453812e25b1d739007474693d20
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-08-17 19:25:16 +1200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM alpine:3.18 as font
+
+RUN apk add --no-cache \
+  curl \
+  unzip
+
+RUN curl https://fonts.google.com/download?family=Montserrat -o /tmp/montserrat.zip &&
+  mkdir -p /usr/share/fonts/ttf-montserrat/ &&
+  unzip /tmp/montserrat.zip -d /usr/share/fonts/ttf-montserrat/ &&
+  rm -rf /tmp/montserrat.zip
+
+FROM alpine:3.18 as generator
+
+RUN apk add --no-cache \
+  inkscape \
+  bash \
+  minify
+
+COPY --from=font /usr/share/fonts/ttf-montserrat/*.ttf /usr/share/fonts/ttf-montserrat/
+
+RUN mkdir /app
+
+COPY --chmod=777 generate.sh template.svg /app
+
+WORKDIR /out
+
+ENTRYPOINT ["/app/generate.sh"]
---
+++ generate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if ! command -v inkscape &>/dev/null; then
+  echo "inkscape could not be found"
+  exit 1
+fi
+
+name=$1
+lower_name=${name,,}
+
+basedir=$(dirname "$0")
+
+text="component-text"
+
+sed s/COMPONENT/$name/ <$basedir/template.svg >/tmp/$lower_name.svg
+
+width=$(inkscape --actions="select-by-id:$text; query-width;" /tmp/$lower_name.svg 2>/dev/null)
+width=$(printf '%.*f' 0 $width)
+
+viewport_width=$(echo "($width + 10)" | bc)
+path_horizontal=$(echo "($width + 5)" | bc)
+
+sed -i s/VIEWPORT_WIDTH/$viewport_width/ /tmp/$lower_name.svg
+sed -i s/PATH_HORIZONTAL/$path_horizontal/ /tmp/$lower_name.svg
+
+inkscape --actions="select-by-id:$text; object-to-path;export-plain-svg;export-filename:/tmp/$lower_name.svg;export-do" /tmp/$lower_name.svg 2>/dev/null
+
+if command -v minify >/dev/null 2>&1; then
+  minify -q -o $lower_name.svg /tmp/$lower_name.svg
+else
+  cp /tmp/$lower_name.svg $lower_name.svg
+fi
